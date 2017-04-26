1c1e1382a305b81630602178d54073e6da9cbf6d
Jack Murphy <jmurphy.web@gmail.com>
2017-04-26 16:35:41 +0100

    remove old component

--- app/index.js
+++ app/index.js
@@ -2,14 +2,11 @@ import React from 'react'
 
 import { render } from 'react-dom'
 
-import Awesome from './Awesome.js'
-
 class App extends React.Component {
   render () {
     return (
       <div>
         <p> Hello react </p>
-        <Awesome />
       </div>
     )
   }
